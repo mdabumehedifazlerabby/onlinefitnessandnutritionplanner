37a8118871731e6669fdbc66514845b87173457e
Abu Mehedi Fazle Rabby <134952091+mdabumehedifazlerabby@users.noreply.github.com>
2023-09-14 23:09:47 +0600

    Update Online Fitness and Nutrition Planner.cpp

--- Online Fitness and Nutrition Planner.cpp
+++ Online Fitness and Nutrition Planner.cpp
@@ -3,38 +3,38 @@
 using namespace std;
 
 class Exercise {
-protected:
+protected:  // Encapsulation: Attributes are protected, accessible to derived classes.
     string name;
     string category;
 
 public:
     Exercise(const string& name, const string& category) : name(name), category(category) {}
 
-    virtual double calculateCaloriesBurned() = 0;
+    virtual double calculateCaloriesBurned() = 0; // Abstraction: Pure virtual function makes Exercise an abstract class.
 };
 
-class StrengthExercise : public Exercise {
+class StrengthExercise : public Exercise { // Inheritance: StrengthExercise is a derived class of Exercise.
 public:
     StrengthExercise(const string& name) : Exercise(name, "Strength") {}
 
-    double calculateCaloriesBurned() override {
+    double calculateCaloriesBurned() override { // Polymorphism: Overriding the pure virtual function from the base class.
         return 150.0;
     }
 };
 
-class CardioExercise : public Exercise {
+class CardioExercise : public Exercise { // Inheritance: CardioExercise is a derived class of Exercise.
 public:
     CardioExercise(const string& name) : Exercise(name, "Cardio") {}
 
-    double calculateCaloriesBurned() override {
+    double calculateCaloriesBurned() override { // Polymorphism: Overriding the pure virtual function from the base class.
         return 200.0;
     }
 };
 
 class PlanInterface {
 public:
-    virtual void createPlan() = 0;
-    virtual void trackProgress() = 0;
+    virtual void createPlan() = 0; // Abstraction: Pure virtual function makes PlanInterface an abstract class.
+    virtual void trackProgress() = 0; // Abstraction: Pure virtual function makes PlanInterface an abstract class.
 };
 
 
@@ -50,4 +50,3 @@ int main() {
 
     return 0;
 }
-
